f563c5d9941b85bc9dc12e961351c7a59e17dd36
Tatiana Frattale <84927172+tatianafrattale@users.noreply.github.com>
2021-08-17 00:42:39 -0400

    department employees table

--- EmployeeSQL/table_schemas.sql
+++ EmployeeSQL/table_schemas.sql
@@ -24,3 +24,15 @@ CREATE TABLE employees (
 -- View employees table
 SELECT *
 FROM employees;
+
+-- Create deparment employees table
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- View department employees table
+SELECT *
+FROM dept_emp;
