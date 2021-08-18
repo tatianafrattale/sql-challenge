8757e62e1de44a9f79f5a73335f0263b55b86566
Tatiana Frattale <84927172+tatianafrattale@users.noreply.github.com>
2021-08-17 22:19:07 -0400

    first query

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,29 @@
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	employees.sex,
+	salaries.salary
+FROM employees
+LEFT JOIN salaries ON
+employees.emp_no = salaries.emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
